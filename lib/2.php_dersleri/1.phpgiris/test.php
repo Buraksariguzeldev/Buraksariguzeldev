9860083b271e579921492a3d2db85a3e90e2bedf
Buraksariguzeldev <buraksariguzeldev@gmail.com>
2024-04-03 19:17:43 +0300

    İndex.html Yenilendi ve resource dosyasida yenilendi

--- lib/2.php_dersleri/1.phpgiris/test.php
+++ lib/2.php_dersleri/1.phpgiris/test.php
@@ -1,14 +1,54 @@
-<?php
-echo "Merhaba, PHP dünyası!";
-echo "Nasıl gidiyor?";
-?>
+<!DOCTYPE html>
+<html lang="tr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP ve HTML Sayfası</title>
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+            margin: 0;
+            padding: 0;
+        }
+        .container {
+            padding: 10px;
+        }
+        .kirmizi-metin {
+            color: red;
+        }
+        hr {
+            border: 1px solid red;
+        }
+        @media only screen and (max-width: 500px) {
+            body {
+         
+  
+            }
+            a {
+                display: block;
+                margin-bottom: 10px;
+            }
+        }
+    </style>
+</head>
+<body>
+    <div class="container">
+<hr />
+        <a href="../../../İndex.html">İndex.html</a>
+<hr />
+        <a href="../../lib.html">Lib</a>
+<hr />
+        <a href="../../../docs/php/phpkullanim.html">Php kullanim</a>
+<hr />
 
-<style>
-    .kirmizi-metin {
-        color: red;
-    }
-</style>
+        <?php
+            echo "Merhaba, PHP dünyası!";
+            echo "Nasıl gidiyor?";
+        ?>
 
-<div>
-    <p class="kirmizi-metin">Bu bir HTML paragrafıdır ve kırmızı renkte yazidir.</p>
-</div>
+        <div>
+            <p class="kirmizi-metin">Bu bir HTML paragrafıdır ve kırmızı renkte yazıdır.</p>
+        </div>
+    </div>
+</body>
+</html>
