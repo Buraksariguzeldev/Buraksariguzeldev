0fec6f7a7701a23e1569fe0e585f4617336484b2
Buraksariguzeldev <buraksariguzeldev@gmail.com>
2024-05-01 19:45:42 +0300

    App, web ve project için bazı değişiklikler yapıldı ve yeni içerikler eklendi.

--- project/1.Sayfa aktarma/script.js
+++
@@ -1,9 +0,0 @@
-function goToPage2() {
-    const htmlContent = $("#htmlInput").val();
-
-    const queryParams = new URLSearchParams();
-    queryParams.set('htmlContent', htmlContent);
-
-    const queryString = queryParams.toString();
-    window.location.href = `page2.html?${queryString}`;
-}
